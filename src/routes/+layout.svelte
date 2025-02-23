e9622eb93e17d020c6a9a8dd813b455e94478d8d
nafi-islam <nafi.islam321@gmail.com>
2025-02-22 20:33:51 -0600

    feat: navbar layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,37 @@
 <script lang="ts">
 	import '../app.postcss';
-	import { AppShell, AppBar } from '@skeletonlabs/skeleton';
+	import { AppShell, AppBar, Modal } from '@skeletonlabs/skeleton';
+	import { getModalStore, initializeStores, LightSwitch } from '@skeletonlabs/skeleton';
+	import type { ModalSettings } from '@skeletonlabs/skeleton';
+	import { Map2 } from 'tabler-icons-svelte';
+	import { QuestionMark } from 'tabler-icons-svelte';
+
+	initializeStores();
+	const modalStore = getModalStore();
+
+	function modalToggle() {
+		const modal: ModalSettings = {
+			type: 'alert',
+			title: 'Help & Instructions',
+			body: 'This is an example modal with game instructions.',
+			image: 'https://i.imgur.com/WOgTG96.gif'
+		};
+		modalStore.trigger(modal);
+		console.log('modal clicked');
+	}
 </script>
+
+<link
+	rel="preload"
+	href="https://fonts.googleapis.com/css2?family=Gloria+Hallelujah&display=swap"
+	as="style"
+/>
+<link
+	rel="stylesheet"
+	href="https://fonts.googleapis.com/css2?family=Gloria+Hallelujah&display=swap"
+/>
+
+<Modal />
 
 <!-- App Shell -->
 <AppShell>
@@ -9,36 +39,39 @@
 		<!-- App Bar -->
 		<AppBar>
 			<svelte:fragment slot="lead">
-				<strong class="text-xl uppercase">Skeleton</strong>
+				<!-- Map Icon & Title -->
+				<div class="flex items-center space-x-2">
+					<Map2 />
+					<strong class="journy-title">journy</strong>
+				</div>
 			</svelte:fragment>
+
 			<svelte:fragment slot="trail">
-				<a
-					class="btn btn-sm variant-ghost-surface"
-					href="https://discord.gg/EXqV7W8MtY"
-					target="_blank"
-					rel="noreferrer"
+				<!-- Dark/Light Mode Toggle -->
+				<LightSwitch />
+
+				<!-- Help Button -->
+				<button
+					class="btn btn-sm variant-ghost-surface flex items-center justify-center w-10 h-10 p-2"
+					on:click={modalToggle}
 				>
-					Discord
-				</a>
-				<a
-					class="btn btn-sm variant-ghost-surface"
-					href="https://twitter.com/SkeletonUI"
-					target="_blank"
-					rel="noreferrer"
-				>
-					Twitter
-				</a>
-				<a
-					class="btn btn-sm variant-ghost-surface"
-					href="https://github.com/skeletonlabs/skeleton"
-					target="_blank"
-					rel="noreferrer"
-				>
-					GitHub
-				</a>
+					<QuestionMark class="w-5 h-5" />
+				</button>
 			</svelte:fragment>
 		</AppBar>
 	</svelte:fragment>
-	<!-- Page Route Content -->
+
+	<!-- Page Content -->
 	<slot />
 </AppShell>
+
+<!-- Import Google Font -->
+<style>
+	@import url('https://fonts.googleapis.com/css2?family=Gloria+Hallelujah&display=swap');
+
+	.journy-title {
+		font-family: 'Gloria Hallelujah', cursive;
+		font-size: 1.5rem;
+		font-weight: bold;
+	}
+</style>
