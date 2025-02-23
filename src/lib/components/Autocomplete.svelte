46aa64b5d90b9b183371428bb88f661b0c567891
nafi-islam <nafi.islam321@gmail.com>
2025-02-23 10:20:28 -0600

    wip!: guess input with autocomplete

---
+++ src/lib/components/Autocomplete.svelte
@@ -0,0 +1,31 @@
+<script lang="ts">
+	import { Autocomplete, popup } from '@skeletonlabs/skeleton';
+	import type { AutocompleteOption, PopupSettings } from '@skeletonlabs/skeleton';
+	import { statesGraph } from '../statesGraph';
+
+	let popupSettings: PopupSettings = {
+		event: 'focus-click',
+		target: 'popupAutocomplete',
+		placement: 'bottom'
+	};
+
+	let inputPopupDemo: string = '';
+
+	function onPopupDemoSelect(event: any): void {
+		inputPopupDemo = event.detail.label;
+	}
+
+	const states = Object.keys(statesGraph).map((state) => ({ label: state, value: state }));
+</script>
+
+<input
+	class="input autocomplete w-full p-3 border rounded-md shadow-md"
+	type="search"
+	name="autocomplete-search"
+	bind:value={inputPopupDemo}
+	placeholder="Search..."
+	use:popup={popupSettings}
+/>
+<div data-popup="popupAutocomplete">
+	<Autocomplete bind:input={inputPopupDemo} options={states} on:selection={onPopupDemoSelect} />
+</div>
